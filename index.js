b4fa2e8c745f266baa38b67b8e5a98d46f6bf322
Natsulelou <nutshapon_lelou@hotmail.com>
2020-03-04 14:18:44 +0700

    morgan log

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var jwt = require('express-jwt');
+var morgan = require('morgan');
 
 var port = 3200;
 
@@ -14,8 +15,14 @@ app.use('/api/', jwt({
     credentialsRequired: false,
 }));
 
+morgan.token('user', function (req, res) {
+    return req.user ? req.user.username: 'guest'
+})
+
+app.use(morgan(':date[web] user=:user method=:method path=:url status=:status response-time=:response-time ms content-length=:res[content-length]'))
+
 app.get('/api/test', function (req, res) {
-    res.send(req.user);
+    res.send('this is get method');
 });
 
 app.post('/api/test', function (req, res) {
